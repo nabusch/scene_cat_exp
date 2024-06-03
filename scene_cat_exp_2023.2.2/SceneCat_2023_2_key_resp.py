8b8522f21fb62f5537917b38b0690ba5fa0c7841
mathilde451 <m.poeppelmann@uni-muenster.de>
2024-06-03 09:48:17 +0200

    deleted unnecessary components

--- scene_cat_exp_2023.2.2/SceneCat_2023_2_key_resp.py
+++ scene_cat_exp_2023.2.2/SceneCat_2023_2_key_resp.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.1.1),
-    on Sun Jun  2 13:56:37 2024
+    on Mon Jun  3 09:46:33 2024
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
